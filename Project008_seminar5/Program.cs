69c5df167fb65d1b7b6cc602410a1c5cf1a7c026
Sergey Antipov <ser_ok@mail.ru>
2023-03-11 17:05:40 +0300

    end of seminar 5

--- Project008_seminar5/Program.cs
+++ Project008_seminar5/Program.cs
@@ -189,48 +189,48 @@
 // [1 2 3 4 5] -> 5 8 3
 // [6 7 3 6] -> 36 21
 
-void FillArray(int[] array)
-{
-    for (int i = 0; i < array.Length; i++)
-    {
-        array[i] = new Random().Next(1, 10);
-    }
-}
+// void FillArray(int[] array)
+// {
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         array[i] = new Random().Next(1, 10);
+//     }
+// }
 
-void PrintArray(int[] array)
-{
-    foreach (int el in array)
-    {
-        Console.Write($"{el} ");
-    }
-    Console.WriteLine();
-}
+// void PrintArray(int[] array)
+// {
+//     foreach (int el in array)
+//     {
+//         Console.Write($"{el} ");
+//     }
+//     Console.WriteLine();
+// }
 
-int[] Sum(int[] array)
-{
-    int[] res = new int[array.Length / 2 + array.Length % 2];
-    for (int i = 0; i < array.Length / 2; i++)
-    {
-        res[i] = array[i] * array[array.Length - 1 - i];
-    }
-    if (array.Length % 2 != 0)
-    {
-        res[array.Length / 2] = array[array.Length / 2];
-    }
+// int[] Sum(int[] array)
+// {
+//     int[] res = new int[array.Length / 2 + array.Length % 2];
+//     for (int i = 0; i < array.Length / 2; i++)
+//     {
+//         res[i] = array[i] * array[array.Length - 1 - i];
+//     }
+//     if (array.Length % 2 != 0)
+//     {
+//         res[array.Length / 2] = array[array.Length / 2];
+//     }
 
-    return res;
+//     return res;
 
-}
+// }
 
 
-Console.Write("Введите количество элементов массива: ");
-int N = Convert.ToInt32(Console.ReadLine());
-int[] array = new int[N];
+// Console.Write("Введите количество элементов массива: ");
+// int N = Convert.ToInt32(Console.ReadLine());
+// int[] array = new int[N];
 
-FillArray(array);
-PrintArray(array);
-int[] result = Sum(array);
-PrintArray(result);
+// FillArray(array);
+// PrintArray(array);
+// int[] result = Sum(array);
+// PrintArray(result);
 
 // =====================
 
